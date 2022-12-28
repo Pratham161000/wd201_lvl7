528a3b02c9448812cf2d012120f60d8a9c4fa20a
Pratham161000 <95738081+Pratham161000@users.noreply.github.com>
2022-12-28 17:14:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-unused-vars */
+const {request, response} = require('express');
+const express = require('express');
+const app = express();
+
+const {Todo} = require('./models');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+//app.use(express.urlencoded({extended: false}));
+
+
+app.use(bodyParser.json());
+
+
+// seting the ejs is the engine
+app.set('view engine', 'ejs');
+
+app.get('/', async (request, response)=>{
+    try{
+    const allTodos = await Todo.getTodos();
+  
+  if (request.accepts('html')) {
+    response.render('index', {
+      allTodos
+    });
+  } else {
+    response.json({allTodos});
+  }
+  }catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/todos', (request, response)=>{
+  console.log('Todo List', request.body);
+});
+app.post('/todos', async (request, response)=>{
+  console.log('Todo List');
+  try {
+    console.log('entering in try block');
+    const todo =await Todo.addTodo({
+      title: request.body.title, dueDate: request.body.dueDate,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put('/todos/:id/markAsCompleted', async (request, response)=>{
+  console.log('We have updated a todo with id:', request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete('/todos/:id', async function(request, response) {
+  console.log('Delete Todo with id: ', request.params.id);
+  const deleteFlag = await Todo.destroy({where: {id: request.params.id}});
+  response.send(deleteFlag ? true : false);
+});
+
+module.exports = app;
